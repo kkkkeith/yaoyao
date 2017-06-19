3c414e5fb6972a9c82f1332721c88edad90c6a01
keith <keithshy@163.com>
2017-06-19 14:09:42 +0800

    Update y3.js

--- IFE-y3/y3.js
+++ IFE-y3/y3.js
@@ -1,14 +1,15 @@
 function init () {
 	var students = document.getElementById("students");
-    var others = document.getElementById("others");
-    var work = document.getElementById("work");
-    var area = document.getElementById("area");
-    var beijing = document.getElementById("beijing");
-    var henan = document.getElementById("henan");
-    var bd = document.getElementById("bd");
-    var qh = document.getElementById("qh");
-    var hd = document.getElementById("hd");
-    var zd = document.getElementById("zd");
+        var others = document.getElementById("others");
+        var work = document.getElementById("work");
+        var area = document.getElementById("area");
+        var beijing = document.getElementById("beijing");
+        var henan = document.getElementById("henan");
+        var bd = document.getElementById("bd");
+        var qh = document.getElementById("qh");
+        var hd = document.getElementById("hd");
+        var zd = document.getElementById("zd");
+	//绑定单击事件
 	students.onclick = function () {
 		work.style.visibility = "hidden";
 		area.style.visibility = "visible";
@@ -18,16 +19,17 @@ function init () {
 		work.style.visibility = "visible";
 	};
 	beijing.onclick = function () {
-	    hd.style.display = "none";
-	    zd.style.display = "none";
+	        hd.style.display = "none";
+	        zd.style.display = "none";
 		bd.style.display = "block";
 		qh.style.display = "block";
+		//设置默认选中
 		qh.selected = true;
-    };
-    henan.onclick = function () {
-	    hd.style.display = "block";
-	    zd.style.display = "block";
-		bd.style.display = "none";
+        };
+        henan.onclick = function () {
+	        hd.style.display = "block";
+	        zd.style.display = "block";
+	   	bd.style.display = "none";
 		qh.style.display = "none";
 		hd.selected = true;
 	};
